b931b9abd8f430fae34e8391dff83cac77930929
Jiyu Jang <apryll7995@gmail.com>
2023-11-29 15:01:13 +0900

    redis url 수정

--- utils/redisConnect.js
+++ utils/redisConnect.js
@@ -1,16 +1,14 @@
 const dotenv = require("dotenv");
 const redis = require("redis");
 
-dotenv.config(); // env환경변수 파일 가져오기
+const url = process.env.REDIS_URI;
 
 const redisClient = redis.createClient({
-  url: `redis://${process.env.REDIS_USERNAME}:${process.env.REDIS_PASSWORD}@${process.env.REDIS_HOST}:${process.env.REDIS_PORT}/0`,
-  legacyMode: true, // 반드시 설정 !!
+  url,
+  legacyMode: true,
 });
 redisClient.on("connect", () => console.info("🟢 Redis 연결 성공!"));
-redisClient.on("error", (err) =>
-  console.error("Redis Client Error", err.message)
-);
+redisClient.on("error", (err) => console.error("Redis Client Error", err.message));
 redisClient.connect();
 
 module.exports = redisClient;
